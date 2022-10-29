94f71feff6b36a317d6b603fff51e9d4ce05732a
AndyyShih <andyclock90108@gmail.com>
2022-10-29 22:19:03 +0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Demo from "./component/deom01";
+
+function App() {
+  return (
+    <div className="App">
+      <Demo />
+    </div>
+  );
+}
+
+export default App;
